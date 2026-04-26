e6abf63fecb1c824506abb4e4e0f50dcf3f2f02e
keiferc <15503711+keiferc@users.noreply.github.com>
2026-04-26 18:03:31 -0400

    increased memory resource limit

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -70,7 +70,7 @@ resource "google_cloud_run_v2_service" "mlflow" {
       resources {
         limits = {
           cpu    = "1"
-          memory = "512Mi"
+          memory = "2Gi"
         }
       }
 
